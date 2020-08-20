812dcf793ce51e40d35c493d91366ddf9308f578
alejandrocolorado <alejandrocolo13@gmail.com>
2020-08-20 08:49:16 -0500

    Set up workspace

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Switch } from "react-router-dom";
+import AuthProvider from "./lib/AuthProvider";
+
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Private from "./pages/Private";
+import AnonRoute from "./components/AnonRoute";
+import PrivateRoute from "./components/PrivateRoute";
+
+class App extends Component {
+  render() {
+    return (
+      <AuthProvider>
+        <div className='container'>
+          <Navbar />
+
+          <Switch>
+            <AnonRoute exact path='/signup' component={Signup} />
+            <AnonRoute exact path='/login' component={Login} />
+            <PrivateRoute exact path='/private' component={Private} />
+          </Switch>
+        </div>
+      </AuthProvider>
+    );
+  }
+}
+
+export default App;
