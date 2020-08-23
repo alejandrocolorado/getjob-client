4039c0c1385e41a44804a8fefd4872b88eee7de7
David Espinosa <davesespinosa@gmail.com>
2020-08-23 19:03:52 +0200

    style added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+
 import { Switch } from "react-router-dom";
 import AuthProvider from "./lib/AuthProvider";
 
 import Navbar from "./components/Navbar";
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
-import Private from "./pages/Private.jsx";
 import AnonRoute from "./components/AnonRoute";
 import PrivateRoute from "./components/PrivateRoute";
 import Profile from "./pages/Profile.jsx";
@@ -17,17 +17,18 @@ class App extends Component {
   render() {
     return (
       <AuthProvider>
-        <div className="container">
-          <Navbar />
+        <div className=" navigate cover wrapper">
+          <div className=" js-content app cover">
 
+          <Navbar/>
           <Switch>
             <AnonRoute exact path="/signup" component={Signup} />
             <AnonRoute exact path="/login" component={Login} />
             <PrivateRoute exact path="/options" component={Options} />
             <PrivateRoute path="/search" component={Search} />
-            <PrivateRoute exact path="/private" component={Private} />
             <PrivateRoute exact path="/profile" component={Profile} />
           </Switch>
+          </div>
         </div>
       </AuthProvider>
     );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 
 ReactDOM.render(
   <Router>
     <App />
-  </Router>
-, document.getElementById('root'));
+  </Router>,
+
+  document.getElementById("root")
+);
