37e1890b2b93015776f4646132b488e1e68681ec
alejandrocolorado <alejandrocolo13@gmail.com>
2020-08-23 12:56:34 -0500

    Added delete function

--- src/pages/PendingJobs.jsx
+++ src/pages/PendingJobs.jsx
@@ -1,56 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { withAuth } from "../lib/AuthProvider";
 import apiService from "./../../src/services/apiService";
 
-
-
 export class PendingJobs extends Component {
-    state = {
-        pendingJobs: []
-    }
+  state = {
+    pendingJobs: [],
+  };
 
-    
-    getPendingJobs = async () => {
-        const pendingJobsObj = await apiService.getAllPendingJobs();
-        //console.log("Aqui------->", pendingJobsObj)
-        this.setState ({
-            pendingJobs: pendingJobsObj.data
-        })
-    }
+  componentDidMount() {
+    this.getPendingJobs();
+  }
 
-    componentDidMount() {
-        this.getPendingJobs();
-      }
+  getPendingJobs = async () => {
+    const pendingJobsObj = await apiService.getAllPendingJobs();
+    console.log("Aqui------->", pendingJobsObj);
+    this.setState({
+      pendingJobs: pendingJobsObj.data,
+    });
+  };
 
+  deleteJob = (jobId) => {
+    console.log(jobId);
+    console.log(this.state);
+    const filtered = this.state.pendingJobs.filter((job) => job._id !== jobId);
+    this.setState({
+      pendingJobs: filtered,
+    });
+  };
 
-    render() {
-        return (
-            <div>
-            <h2>Pending Job Applications</h2>
-            <div>
-              {this.state.pendingJobs.map((job) => {
-                  
-                return (
-                  <div key={job._id}>
-                    <Link to={`/job-detail/${job._id}`}>
-                      <h3>{job.title}</h3>
-                      <p>{job.company_name}</p>
-                      <ul>
-                          {job.technologies.map((tag) =>{
-                              return (<li>{tag.name}</li>)
-                          })}
-                      </ul>
-                    </Link>
-                    {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
-                  </div>
-                );
-              })}
-            </div>
+  render() {
+    return (
+      <div>
+        <h2>Pending Job Applications</h2>
+        <div>
+          {this.state.pendingJobs.map((job) => {
+            return (
+              <div key={job._id}>
+                <Link to={`/job-detail/${job._id}`}>
+                  <h3>{job.title}</h3>
+                </Link>
+                <p>{job.company_name}</p>
+                <ul>
+                  {job.technologies.map((tag) => {
+                    return <li>{tag.name}</li>;
+                  })}
+                </ul>
+                <button onClick={() => this.deleteJob(job._id)}>Delete</button>
 
-          </div>
-        )
-    }
+                {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withAuth(PendingJobs)
+export default withAuth(PendingJobs);
